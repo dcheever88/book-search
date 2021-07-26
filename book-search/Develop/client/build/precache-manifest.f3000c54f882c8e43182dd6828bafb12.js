0af9110de6de8efe35b1985b45e233483c8e7de3
dcheever <cheeverdanny@gmail.com>
2021-07-26 18:45:47 -0500

    forgot to build in root maybe?

---
+++ book-search/Develop/client/build/precache-manifest.f3000c54f882c8e43182dd6828bafb12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dac6117711c455315e3b34f8b59aaeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de494845b833b4481550",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "1125aeb7f62b2e4d05cb",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "de494845b833b4481550",
+    "url": "/static/js/2.485a1a82.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.485a1a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1125aeb7f62b2e4d05cb",
+    "url": "/static/js/main.179bacc3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
