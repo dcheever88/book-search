e05d72b63c7eed0b565fb901de570a11299eff33
dcheever <cheeverdanny@gmail.com>
2021-07-26 19:19:27 -0500

    reinstalled graphql

--- book-search/Develop/server/utils/auth.js
+++ book-search/Develop/server/utils/auth.js
@@ -1,10 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'supersecret';
+const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
   // function for our authenticated routes
   authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
@@ -28,11 +34,6 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return req;
   },
 };
